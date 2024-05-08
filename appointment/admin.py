0b50d88cc2d4287cffbb2cecd8dc630832e868d4
Jisan Mahmud <jisa.mahmud20@gmail.com>
2024-05-08 16:15:12 +0600

    Update appointment app

--- appointment/admin.py
+++ appointment/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from appointment.models import Appointment
 # Register your models here.
 
-admin.site.register(Appointment)
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ['patient', 'doctor', 'time']
+    list_filter = ['appointment_status', 'appointment_type']
+    search_fields = ['patient__user__first_name', 'doctor__user__first_name', 'appointment_status', 'appointment_type']
--- appointment/views.py
+++ appointment/views.py
@@ -5,3 +5,10 @@ from .models import Appointment
 class AppointmentViewset(viewsets.ModelViewSet):
     queryset = Appointment.objects.all()
     serializer_class = AppointmentSerializer
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        patient_id = self.request.query_params.get('patient_id', None)
+        if patient_id != None:
+            queryset = queryset.filter(patient__id = patient_id)
+        return queryset
